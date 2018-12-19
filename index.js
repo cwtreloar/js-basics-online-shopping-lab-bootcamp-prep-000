06ee9b3a047aa64b9c27986683dac516ffb5b3b6
Chad Treloar <cwtreloar@gmail.com>
2018-12-19 05:15:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -91,6 +91,9 @@ function total() {
 }
 
 function removeFromCart(item) {
+  if (cart[i].itemName != item) {
+    return `That item is not in your cart.`;
+  }
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1);
